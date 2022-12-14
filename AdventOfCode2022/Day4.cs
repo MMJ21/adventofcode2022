0cc9946c2c775c87c6bca05045d062fedda706d9
Marcos Martínez Játiva <marcos.martinez-jativa@capgemini.com>
2022-12-14 12:17:00 +0100

    Day4 Problem1

---
+++ AdventOfCode2022/Day4.cs
@@ -0,0 +1,30 @@
+namespace AdventOfCode.Days
+{
+    public static class Day4
+    {
+        private static Func<string, int[]> ConvertIntoIntArray = (string s) => 
+        {
+            var numbers = s.Split("-");
+            return new int[] { Int32.Parse(numbers[0]), Int32.Parse(numbers[1]) };
+        };
+        public static int Problem1(string[] input)
+        {
+            var result = 0;
+
+            foreach (var rawPair in input)
+            {
+                var pair = rawPair.Split(",");
+                var first = ConvertIntoIntArray(pair[0].TrimStart());
+                var second = ConvertIntoIntArray(pair[1].TrimEnd());
+
+                if ((first[0] <= second[0] && first[1] >= second[1])
+                    || (first[0] >= second[0] && first[1] <= second[1]))
+                {
+                    result++;
+                }
+            }
+
+            return result;
+        }
+    }
+}
